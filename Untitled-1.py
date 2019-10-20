c6836e604cb60218f33088ad7f4cae42191b35a0
mhkhansahab <hamza.khansahab@gmail.com>
2019-10-20 22:12:48 +0500

    Second Commit

---
+++ Untitled-1.py
@@ -0,0 +1,4 @@
+print("Hello World")
+list=["hamza", 9, "098, 87.0"]
+for a in list:
+    print(a)
---
+++ mymath.py
@@ -0,0 +1,10 @@
+def add(x,y):
+    z = (x + y)
+    return z
+
+def sub(x,y):
+    z = (x-y)
+    return z
+
+
+print("mymath.py module executed")
